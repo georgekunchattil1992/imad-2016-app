a2af35a525d87a9c288eff728928d804698efae5
georgekunchattil1992 <georgekunchattil@gmail.com>
2016-10-06 07:19:27 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-var morgan = require('morgan');
-var path = require('path');
-
 var express=require('express');
-var app = express();
-
-app.use(morgan('combined'));
+var app=express();
 
 app.use(express.static('public'));
 app.get('/',function(req,res){
@@ -16,19 +11,6 @@ app.get('/css/style.css',function(req,res){
 	res.sendFile(__dirname + '/css/style.css');
 });*/
 
-/*app.get('/ui/css/bootstrap.min.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '/css/bootstrap.min.css'));
-});*/
-/*app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});*/
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+app.listen(8080,function(){
+	console.log('app is listening on port 192.168.1.3:8080 !');
 });
-
-
-
-
