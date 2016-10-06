a8adacf28ecba4fdf6a4c2f36afec5db048615c7
georgekunchattil1992 <georgekunchattil@gmail.com>
2016-10-06 12:25:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var express=require('express');
 var app=express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'/public')));
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/public/index.html');
 });
